3b7f1459bdda6eedb04fa20e7b8273118fd8e3f9
Tom <tom.aschmann@t-online.de>
2020-03-21 18:35:40 +0100

    PHP-Dateien hinzufügen

---
+++ inc/header.inc.php
@@ -0,0 +1,12 @@
+<body>
+
+  <!-- Header -->
+    <section id="header">
+      <header>
+        <h1>Overflow</h1>
+        <p>By HTML5 UP</p>
+      </header>
+      <footer>
+        <a href="#banner" class="button style2 scrolly-middle">Proceed as anticipated</a>
+      </footer>
+    </section>
