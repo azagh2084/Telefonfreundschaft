db5aaaf71e9e98196a664aa1ef693363b42d48da
Tom <tom.aschmann@t-online.de>
2020-03-22 11:55:22 +0100

    Footer Anpassungen und Globele Variablen Datei

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -5,7 +5,7 @@
   </ul>
   <div class="copyright">
     <ul class="menu">
-      <li>&copy;2020 Gruppe die noch keinen festen Namen hat #WirVsVirus (Webseite ist gehostet bei Tom A.)</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+      <li>&copy;<?php echo date('Y').$site_name; ?> <a href="https://wirvsvirushackathon.org/" rel="external" target="_blank" title="Wir gegen COVID-19">#WirVsVirus</a></li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li><li><a href="https://github.com/azagh2084/Telefonfreundschaft" target="_blank" rel="sourcecode" title="Schaue dir den Sourcecode auf GitHub an.">GitHub</a></li>
     </ul>
   </div>
 </section>
