74fa00a75de5564c05d03b80488ca1b723449e3a
Sebastian Seggewiß <s.seggewiss@shopware.com>
2020-03-22 09:16:25 +0100

    Refactor database credentials + added logging

--- inc/database.inc.php
+++ inc/database.inc.php
@@ -1,17 +1,20 @@
 <?php declare(strict_types=1);
 
+@include(__DIR__ . '/database.rm.inc.php');
+if (!isset($username)) {
+    $username = '';
+    $password = '';
+}
+
 $dsn = "mysql:host=127.0.0.1;dbname=wirvsvirus;charset=utf8mb4";
 $options = [
     PDO::ATTR_EMULATE_PREPARES   => false, // turn off emulation mode for "real" prepared statements
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, //turn on errors in the form of exceptions
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, //make the default fetch be an associative array
 ];
-@include('/inc/database.rm.inc.php');
-$mysql_username = '';
-$mysql_password = '';
+
 try {
-    $connection = new \PDO($dsn, $mysql_username, $mysql_password, $options);
+    $connection = new \PDO($dsn, $username, $password, $options);
 } catch (\Exception $e) {
-    // ToDo enable die on server
-    // die('Could not establish connection to database.');
+    error_log(print_r($e->getMessage(), true), 3, sprintf(__DIR__ . '/../log/%s.log', time()));
 }
