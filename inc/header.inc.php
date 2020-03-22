46badf9a1f5584f96279c958900161edac25116b
Tom <tom.aschmann@t-online.de>
2020-03-22 01:07:00 +0100

    Link zu Hack seite hinzugefügt

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -4,7 +4,7 @@
     <section id="header">
       <header>
         <h1>Telefonieren gegen Einsamkeit</h1>
-        <p>#WirVsVirus</p>
+        <p><a href="https://wirvsvirushackathon.org/" rel="external" title="Wir gegen COVID-19">#WirVsVirus</a></p>
       </header>
       <footer>
         <a href="#banner" class="button style2 scrolly-middle">Jetzt starten</a>
