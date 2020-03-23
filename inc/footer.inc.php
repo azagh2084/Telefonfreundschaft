91a11c349edc22cd0cafc2ded75812d0f5b45b02
Tom <tom.aschmann@t-online.de>
2020-03-23 20:10:01 +0100

    Kleine änderung

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -2,10 +2,12 @@
   <ul class="icons">
     <li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter (comming soon)</span></a></li>
     <li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram (comming soon)</span></a></li>
+    <li><a href="#" class="icon brands fa-youtube"><span class="label">Youtube</span></a></li>
   </ul>
   <div class="copyright">
     <ul class="menu">
-      <li>&copy;<?php echo date('Y').' '.$site_name; ?> <a href="https://wirvsvirushackathon.org/" rel="external" target="_blank" title="Wir gegen COVID-19">#WirVsVirus</a></li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li><li><a href="https://github.com/azagh2084/Telefonfreundschaft" target="_blank" rel="sourcecode" title="Schaue dir den Sourcecode auf GitHub an.">GitHub</a></li>
+      <li>&copy;<?php echo date('Y').' '.$site_name; ?> <a href="https://wirvsvirushackathon.org/" rel="external" target="_blank" title="Wir gegen COVID-19">#WirVsVirus</a></li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+      <li><a href="https://github.com/azagh2084/Telefonfreundschaft" target="_blank" rel="sourcecode" title="Schaue dir den Sourcecode auf GitHub an.">GitHub</a></li>
     </ul>
   </div>
 </section>
